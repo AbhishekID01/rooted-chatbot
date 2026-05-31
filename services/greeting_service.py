b576f90485bed1080010e8724e0f17bbda4c52de
AbhishekID01 <deshbandariabhishek@gmail.com>
2026-05-31 13:59:31 +0530

    Fix greeting service

--- services/greeting_service.py
+++ services/greeting_service.py
@@ -1,19 +1,16 @@
 def get_greeting(message):
-
-```
 greetings = [
-    "hi",
-    "hello",
-    "hey",
-    "heyy",
-    "hii",
-    "start",
-    "good morning",
-    "good evening"
+"hi",
+"hello",
+"hey",
+"heyy",
+"hii",
+"start",
+"good morning",
+"good evening"
 ]
 
 if message.lower().strip() in greetings:
     return "WELCOME_MENU"
 
 return None
-```
